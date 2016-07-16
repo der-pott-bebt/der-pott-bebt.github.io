21020435c65ce2c59192484c9b061944fed076b5
enigmoe <mz1997@hotmail.de>
2016-07-16 20:56:39 +0200

    edited the magnitude graph - it now has the right scale, colors and mouseover-color

--- js/charts_original.js
+++ js/charts_original.js
@@ -4,11 +4,10 @@ $(function() {
             type: 'column'
         },
         title: {
-            text: 'Erdbeben im Wochenverlauf'
+            text: 'Anzahl Erdbeben pro Wochentag'
         },
         subtitle: {
-            text: 'Zeitraum: 1983-2016 | <a href="http://www.gmg.ruhr-uni-bochum.de/geophysik/seisobs/index.html"> Daten: Seismologisches Observatorium der Ruhr-Universität Bochum</a>',
-            useHTML: true
+            text: '1983-2016 | Quelle: xx'
         },
         xAxis: {
             categories: [
@@ -26,20 +25,16 @@ $(function() {
             min: 0,
             title: {
                 text: 'Anzahl Erdbeben'
-            },
-            labels: {
-              format: '{value}'
             }
         },
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.f} Erdbeben</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:.1f} Erdbeben</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
         },
-
         legend: {
             enabled: false,
         },
@@ -51,8 +46,9 @@ $(function() {
                 shadow: true,
                 color: "#b20902"
             },
-            labels: {
-              format:{value:".f"}
+            yAxis.labels.style: {
+              textOverflow: 'none'
+            }
             }
         },
         series: [{
@@ -68,11 +64,10 @@ $(function() {
             type: 'column'
         },
         title: {
-            text: 'Verteilung der Erdbeben im Tagesverlauf'
+            text: 'Vertelung der Erdbeben im Tagesverlauf'
         },
         subtitle: {
-            text: 'Zeitraum: 1983-2016 | Quelle: Seismologisches Observatorium der Ruhr-Universität Bochum',
-            useHTML: true
+            text: 'Quelle: xx'
         },
         xAxis: {
             categories: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
@@ -90,7 +85,7 @@ $(function() {
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.f} Erdbeben</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:.1f} Erdbeben</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
@@ -121,32 +116,28 @@ $(function() {
             $('#chart_magnitude').highcharts({
                     chart: {
                         type: 'column'
-                        },
+                    },
                     title: {
                         text: 'Stärke der Erdbeben'
                     },
                     subtitle: {
-                        text: '1983-2016 | Daten: Seismologisches Observatorium der Ruhr-Universität Bochum',
-                        useHTML: true
+                        text: '1983-2016'
                     },
                     xAxis: {
-                        /*categories: ["1.0","1.1","1.2","1.3","1.4","1.5","1.6",
-                                "1.7","1.8","1.9","2.0","2.1","2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2", "3.3", "3.4", "3.5", "3.6","3.7","3.8", "3.9","4.0"],*/
-                            crosshair: false,
-                            title: {
-                              text: 'Magnitude'
-                            }
+                        categories: ["1.0","1.1","1.2","1.3","1.4","1.5","1.6",
+                                "1.7","1.8","1.9","2.0","2.1","2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2", "3.3", "3.4", "3.5", "3.6","3.7","3.8", "3.9","4.0"],
+                            crosshair: false
                         },
                         yAxis: {
                             min: 0,
                             title: {
-                                text: 'Anzahl Erdbeben'
+                                text: 'Magnitude'
                             }
                         },
                         tooltip: {
                             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                                '<td style="padding:0"><b>{point.y:.f} Erdbeben</b></td></tr>',
+                                '<td style="padding:0"><b>{point.y:.1f} Erdbeben</b></td></tr>',
                             footerFormat: '</table>',
                             shared: true,
                             useHTML: true
@@ -160,32 +151,14 @@ $(function() {
                                 groupPadding: 0.06,
                                 borderWidth: 0,
                                 shadow: true,
-                                states: {
-                                  hover: {
-                                    color: '#fd6d67',
-                                    brightness: 0.5
-                                  }
-                                }
+                                colors: ["#fd5b54","#fd5b54","#fd5b54","#fd5b54","#fd5b54","#fd5b54","#fd4840","#fd4840","#fd4840","#fd352d","#fd352d","#fd352d","#fc231a","#fc231a","#fc231a","#fc1006","#fc1006","#fc1006"]
                             }
 
                         },
-                        //colors: { ["#fd5b54","#fd5b54","#fd5b54","#fd5b54","#fd5b54","#fd5b54","#fd4840","#fd4840","#fd4840","#fd4840","#fd4840","#fd4840","#fd352d","#fd352d","#fd352d","#fd352d","#fd352d","#fd352d","#fc231a","#fc231a","#fc231a","#fc231a","#fc231a","#fc231a","#fc1006","#fc1006","#fc1006","#fc1006","#fc1006","#fc1006","#b20902"]//
-                      //},//
                         series: [{
-                            name: 'Erdbeben',
-                            data: [[1.0,1849],[1.1,1857],[1.2,1769],[1.3,1607],[1.4,1549],[1.5,1357],[1.6,1030],[1.7,833],[1.8,571],[1.9,373],[2.0,237],[2.1,175],[2.2,162],[2.3,79],[2.4,58],[2.5,33],[2.6,21],[2.7,16],[2.8,15],[2.9,10],[3.0,9],[3.1,2],[3.2,1],[3.3,3],[3.4,0],[3.5,2],[3.6,1],[3.7,1],[3.8,0],[3.9,0],[4.0,0]
-                            ],
-                            zoneAxis: 'x',
-                            zones: [{
-                              value: 2,
-                              color: '#fd5b54'
-                            },{
-                                value: 3,
-                                color: '#fc231a'
-                              },{
-                                value: 4,
-                                color: '#b20902'
-                              }]
+                            name: 'x',
+                            data: [1849,1857,1769,1607,1549,1357,1030,833,571,373,237,175,162,79,58,33,21,16,15,10,9,2,1,3,0,2,1,1,0,0,0
+                      				]
                         }]
                     });
             });
